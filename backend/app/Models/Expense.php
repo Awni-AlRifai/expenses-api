401c4d0b1154f37096a376b6a91d3aade40c366d
Awni-AlRifai <Awni.alrifai@hearst.com>
2023-05-12 23:34:16 +0300

    Add expenses model and relations

--- backend/app/Models/Expense.php
+++ backend/app/Models/Expense.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Expense extends Model
 {
     use HasFactory;
 
@@ -14,8 +14,8 @@ class Category extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function expenses()
+    public function category()
     {
-        return $this->hasMany(Expense::class);
+        return $this->belongsTo(Category::class);
     }
 }
