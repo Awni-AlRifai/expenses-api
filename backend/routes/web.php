5b970462d0455ed090f7a479cbbddd757412d142
Awni-AlRifai <Awni.alrifai@hearst.com>
2023-05-14 14:16:30 +0300

    Refactor code

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'data' => "Welcome to the expenses api if you want to see the api doc go to /api/swagger",
+    ]);
 });
